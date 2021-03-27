051801a7a1a9fe36d317dee1e7607a75aba870ec
xKirtle <xkirtle26@gmail.com>
2021-03-27 18:13:08 +0000

    Added a way to input numbers to the board and more
    Added functionality to all buttons
    Selecting a square on the board will highlight it's group, row and column.

--- assets/js/sudoku.js
+++ assets/js/sudoku.js
@@ -13,6 +13,10 @@ let grid = document.getElementById("grid");
             cell.classList.add("Cell");
             cell.style.gridArea = "Cell" + j;
 
+            cell.addEventListener("mousedown", () => {
+                setSelectedCell(cell);
+            });
+
             let cellValue = document.createElement("div");
             cellValue.classList.add("CellValue");
             cellValue.style.gridArea = "1 / 1 / 4 / 4";
@@ -55,6 +59,10 @@ let grid = document.getElementById("grid");
         numbers.style.gridArea = "N" + i;
         numbers.textContent = i;
 
+        numbers.addEventListener("mousedown", () => {
+            setSelectedCellValue(i);
+        });
+
         controlNumbers.append(numbers);
     }
 
@@ -65,18 +73,121 @@ let grid = document.getElementById("grid");
     controlOptions.classList.add("ControlOptions");
 
     let pencil = document.createElement("div");
-    pencil.classList.add("Modes");
+    pencil.classList.add("Numbers");
+    pencil.style.paddingTop = "40px";
     pencil.textContent = "Pencil";
     pencil.style.gridArea = "Pencil";
+    pencil.addEventListener("mousedown", () => {
+        isPencilEnabled = !isPencilEnabled;
+
+        if (isPencilEnabled)
+            pencil.classList.add("CellActive");
+        else
+            pencil.classList.remove("CellActive");
+    });
 
     controlOptions.append(pencil);
 
     let eraser = document.createElement("div");
-    eraser.classList.add("Modes");
+    eraser.classList.add("Numbers");
+    eraser.style.paddingTop = "40px";
     eraser.textContent = "Erase";
     eraser.style.gridArea = "Eraser";
+    eraser.addEventListener("mousedown", () => {
+        setSelectedCellValue(null);
+    });
 
     controlOptions.append(eraser);
 
     options.append(controlOptions);
+}
+
+//TODO: Figure out a way to mark "readOnly" fields from the sudoku puzzle
+
+let selectedCell = null;
+let isPencilEnabled = false;
+
+function setSelectedCell(element) {
+    if (!element) return;
+
+    if (selectedCell != null) {
+        selectedCell.classList.remove("CellActive");
+    }
+
+    selectedCell = element;
+    highlightSelectedCell();
+    selectedCell.classList.add("CellActive");
+}
+
+function highlightSelectedCell() {
+    //No need to do additional checks here due to call order
+
+    //Group
+    selectedCell.parentElement.classList.add("CellSubActive");
+
+    //Find current group/cell index
+    let groupIndex = -1;
+    let cellIndex = -1;
+    for (let i = 0; i < 9; i++) {
+        if (selectedCell.parentElement == grid.children[i]) {
+            groupIndex = i;
+
+            for (let j = 0; j < 9; j++) {
+                if (selectedCell == grid.children[i].children[j])
+                    cellIndex = j;
+            }
+        }
+    }
+    //How can it ever be -1?
+    if (groupIndex == -1 || cellIndex == -1) return;
+
+    //Clear board beforehand
+    for (let i = 0; i < 9; i++) {
+        //Groups
+        grid.children[i].classList.remove("CellSubActive");
+
+        //Cells
+        for (let j = 0; j < 9; j++)
+            grid.children[i].children[j].classList.remove("CellSubActive");
+    }
+
+    //Row
+    //let row = Math.trunc(groupIndex / 3) * 3 + Math.trunc(cellIndex / 3);
+    let groupRow = Math.trunc(groupIndex / 3) * 3;
+    let cellRow = Math.trunc(cellIndex / 3) * 3;
+
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++)
+            grid.children[groupRow + i].children[cellRow + j].classList.add("CellSubActive");
+    }
+
+    //Column
+    //let column = (groupIndex % 3) * 3 + cellIndex % 3;
+    let groupColumn = groupIndex % 3;
+    let cellColumn = cellIndex % 3;
+
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++)
+            grid.children[groupColumn + (i * 3)].children[cellColumn + (j * 3)].classList.add("CellSubActive");
+    }
+}
+
+function setSelectedCellValue(value) {
+    if (selectedCell == null) return;
+
+    //Eraser will set the value to null
+    if (!isPencilEnabled || value == null) {
+        //Remove any pencil notes that could be present
+        for (let i = 1; i < 10; i++)
+            selectedCell.children[i].classList.add("hidden-pen");
+        selectedCell.children[0].innerHTML = value;
+    } else {
+        //Remove any value that could be present
+        selectedCell.children[0].innerHTML = null;
+
+        if (selectedCell.children[value].classList.contains("hidden-pen"))
+            selectedCell.children[value].classList.remove("hidden-pen");
+        else
+            selectedCell.children[value].classList.add("hidden-pen");
+    }
 }
