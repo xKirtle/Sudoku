ac368aaf92955017cbadb1a086d037a65170829e
xKirtle <xkirtle26@gmail.com>
2021-03-29 11:51:45 +0100

    The selected cell now highlights cells with the same value in other groups
    Backend logic now relies on JS objects instead of the values present in the html elements.

--- assets/js/sudoku.js
+++ assets/js/sudoku.js
@@ -1,14 +1,28 @@
 let grid = document.getElementById("grid");
+let board = [];
+
+//TODO: Make readOnly fields a different a text color
 
 { //Board generation
     //Create Groups
     for (let i = 1; i < 10; i++) {
+        let groupObj = {
+            cells: []
+        };
+
         let group = document.createElement("div");
         group.classList.add("Group");
         group.style.gridArea = "Group" + i;
 
         //Create Cells
         for (let j = 1; j < 10; j++) {
+            let cellObj = {
+                value: null,
+                readOnly: false,
+                pencilEnabled: false,
+                pencilValues: [0, 0, 0, 0, 0, 0, 0, 0, 0]
+            };
+
             let cell = document.createElement("div");
             cell.classList.add("Cell");
             cell.style.gridArea = "Cell" + j;
@@ -22,8 +36,10 @@ let grid = document.getElementById("grid");
             cellValue.style.gridArea = "1 / 1 / 4 / 4";
             //cellValue.style.gridArea = "Pen5";
             //cellValue.classList.add("hidden-pen");
-            if (Math.random() >= 0.7)
+            if (Math.random() >= 0.7) {
+                cellObj.value = j;
                 cellValue.textContent = j;
+            }
 
             cell.append(cellValue);
 
@@ -38,9 +54,11 @@ let grid = document.getElementById("grid");
                 cell.append(pen);
             }
 
+            groupObj.cells.push(cellObj);
             group.append(cell);
         }
 
+        board.push(groupObj);
         grid.append(group);
     }
 
@@ -102,32 +120,20 @@ let grid = document.getElementById("grid");
     options.append(controlOptions);
 }
 
-//TODO: Figure out a way to mark "readOnly" fields from the sudoku puzzle
-
 let selectedCell = null;
+let groupIndex = -1;
+let cellIndex = -1;
 let isPencilEnabled = false;
 
 function setSelectedCell(element) {
     if (!element) return;
 
-    if (selectedCell != null) {
-        selectedCell.classList.remove("CellActive");
-    }
-
     selectedCell = element;
+    getSelectedCellInfo();
     highlightSelectedCell();
-    selectedCell.classList.add("CellActive");
 }
 
-function highlightSelectedCell() {
-    //No need to do additional checks here due to call order
-
-    //Group
-    selectedCell.parentElement.classList.add("CellSubActive");
-
-    //Find current group/cell index
-    let groupIndex = -1;
-    let cellIndex = -1;
+function getSelectedCellInfo() {
     for (let i = 0; i < 9; i++) {
         if (selectedCell.parentElement == grid.children[i]) {
             groupIndex = i;
@@ -138,8 +144,10 @@ function highlightSelectedCell() {
             }
         }
     }
-    //How can it ever be -1?
-    if (groupIndex == -1 || cellIndex == -1) return;
+}
+
+function highlightSelectedCell() {
+    //No need to do additional checks here due to call order
 
     //Clear board beforehand
     for (let i = 0; i < 9; i++) {
@@ -147,9 +155,14 @@ function highlightSelectedCell() {
         grid.children[i].classList.remove("CellSubActive");
 
         //Cells
-        for (let j = 0; j < 9; j++)
+        for (let j = 0; j < 9; j++) {
+            grid.children[i].children[j].classList.remove("CellActive");
             grid.children[i].children[j].classList.remove("CellSubActive");
+        }
     }
+
+    //Group
+    selectedCell.parentElement.classList.add("CellSubActive");
 
     //Row
     //let row = Math.trunc(groupIndex / 3) * 3 + Math.trunc(cellIndex / 3);
@@ -170,24 +183,91 @@ function highlightSelectedCell() {
         for (let j = 0; j < 3; j++)
             grid.children[groupColumn + (i * 3)].children[cellColumn + (j * 3)].classList.add("CellSubActive");
     }
+
+    selectedCell.classList.add("CellActive");
+
+    let selCellData = board[groupIndex].cells[cellIndex];
+    if (selCellData.value == null) return;
+
+    //Find numbers in other groups that match the selected cell
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            //Ignore selected cell
+            if (i == groupIndex && j == cellIndex) continue;
+
+            let tempCellData = board[i].cells[j];
+            if (tempCellData.value != null && tempCellData.value == selCellData.value) {
+                grid.children[i].children[j].classList.add("CellActive");
+            }
+        }
+    }
 }
 
 function setSelectedCellValue(value) {
-    if (selectedCell == null) return;
+    let selCellData = board[groupIndex].cells[cellIndex];
+    if (selectedCell == null || selectedCell.readOnly) return;
 
     //Eraser will set the value to null
     if (!isPencilEnabled || value == null) {
-        //Remove any pencil notes that could be present
-        for (let i = 1; i < 10; i++)
-            selectedCell.children[i].classList.add("hidden-pen");
-        selectedCell.children[0].innerHTML = value;
-    } else {
-        //Remove any value that could be present
-        selectedCell.children[0].innerHTML = null;
+        selCellData.value = value;
 
-        if (selectedCell.children[value].classList.contains("hidden-pen"))
-            selectedCell.children[value].classList.remove("hidden-pen");
-        else
-            selectedCell.children[value].classList.add("hidden-pen");
+        if (selCellData.pencilEnabled) {
+            selCellData.pencilValues = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+            selCellData.pencilEnabled = false;
+        }
+    } else {
+        selCellData.value = null;
+        selCellData.pencilEnabled = true;
+        selCellData.pencilValues[value - 1] = Number(!selCellData.pencilValues[value - 1]);
+    }
+
+    highlightSelectedCell();
+    renderCellValues();
+}
+
+function renderCellValues() {
+    //Updates the values of all the cells to match what's in the board array
+
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            let tempCell = grid.children[i].children[j];
+            let tempCellData = board[i].cells[j];
+
+            //Remove previous data in the cell
+            tempCell.children[0].textContent = null;
+            for (let k = 1; k < 10; k++)
+                tempCell.children[k].classList.add("hidden-pen");
+
+            if (!tempCellData.pencilEnabled) {
+                if (!tempCellData.readOnly)
+                    tempCell.children[0].textContent = tempCellData.value;
+            } else {
+                for (let k = 0; k < 9; k++) {
+                    if (tempCellData.pencilValues[k])
+                        tempCell.children[k + 1].classList.remove("hidden-pen");
+                }
+            }
+        }
+    }
+}
+
+//Unused?
+function renderCellValue() {
+    //Updates the value of the selectedCell in the html element
+
+    let selCellData = board[groupIndex].cells[cellIndex];
+    //Remove previous data in the selected cell
+    selectedCell.children[0].textContent = null;
+    for (let i = 1; i < 10; i++)
+        selectedCell.children[i].classList.add("hidden-pen");
+
+    if (!isPencilEnabled) {
+        if (!selCellData.readOnly)
+            selectedCell.children[0].textContent = selCellData.value;
+    } else {
+        for (let i = 0; i < 9; i++) {
+            if (selCellData.pencilValues[i])
+                selectedCell.children[i + 1].classList.remove("hidden-pen");
+        }
     }
 }
